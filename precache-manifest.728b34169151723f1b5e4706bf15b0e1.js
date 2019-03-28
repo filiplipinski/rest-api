3a9961bfb0eca121a3e68149d3097c6e83575834
filiplipinski <filip.lipinski@outlook.com>
2019-03-28 11:04:39 +0100

    Updates

---
+++ precache-manifest.728b34169151723f1b5e4706bf15b0e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a4a66c2d79fb849ea78",
+    "url": "/zad_euvic/static/js/runtime~main.3a0ed824.js"
+  },
+  {
+    "revision": "6ab50107ffb7b9a8163b",
+    "url": "/zad_euvic/static/js/main.68fafecf.chunk.js"
+  },
+  {
+    "revision": "61abc21708a3b31480a4",
+    "url": "/zad_euvic/static/js/2.18c93355.chunk.js"
+  },
+  {
+    "revision": "6ab50107ffb7b9a8163b",
+    "url": "/zad_euvic/static/css/main.a5a73186.chunk.css"
+  },
+  {
+    "revision": "70b991f831d03345021594c917cdbee9",
+    "url": "/zad_euvic/index.html"
+  }
+];
